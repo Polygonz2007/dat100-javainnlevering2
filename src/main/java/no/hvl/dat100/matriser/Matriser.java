b9aef96006672023df7cfd942b2ad7c2b41ac01d
skfroystein@outlook.com <skfroystein@outlook.com>
2026-09-23 15:35:50 +0200

    Matrix works except reflect

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -38,33 +38,72 @@ public class Matriser {
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
+		int height = matrise.length;
+		int width = matrise[0].length;
+
+		String str = "";
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				str += matrise[y][x];
+				if (x != width - 1 ) {  str += " "; }
+			}
+			str += "\n";
+		}
+
+		return str;
 		
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+
+		int height = matrise.length;
+		int width = matrise[0].length;
+
+		int[][] new_matrix = new int[height][width];
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				new_matrix[y][x] = matrise[y][x] * tall;
+			}
+		}
+
+		return new_matrix;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
+		int height = a.length;
+		int width = a[0].length;
+
+		if (height != b.length || width != b[0].length)
+			return false;
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (a[y][x] != b[y][x]) return false;
+			}
+		}
+
+		return true;
 		
 	}
 	
 	// e)
 	public static int[][] speile(int[][] matrise) {
 
-		// TODO
+		int height = matrise.length;
+		int width = matrise[0].length;
 
-		throw new UnsupportedOperationException("Metoden speile ikke implementert");
+		int[][] new_matrix = new int[matrise.length][matrise[0].length];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				new_matrix[y][x] = matrise[height - y - 1][width - x - 1];
+			}
+		}
+
+		return new_matrix;
 	
 	}
 
